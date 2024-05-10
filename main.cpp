bbc56c91181aeb009bd2c4944bc07d79e469b7b3
KalebSand <157537090+KalebSand@users.noreply.github.com>
2024-05-09 23:12:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,23 +1,55 @@
-#include "movielist.h"
-#include <fstream>
-#include<iostream>
+#pragma once
+#pragma once
+
+#include <string>
 
 using namespace std;
 
-int main() {
-    ifstream inputFile;
-    inputFile.open("movies.txt");
+const int MAX_REVIEW_LENGTH = 30;
+const int TABLE_SIZE = 8; // Adjust the table size based on your requirements
 
-    if (!inputFile.is_open())
-        cout << "File: movies.txt is not found" << endl;
+struct Movie {
+    string name;
+    int rating;
+    double releaseYear;
+    string review;
+    string genre;
+    Movie* next;
 
+    Movie() : rating(0), releaseYear(0), next(nullptr) {}
 
-    MovieList movieListSystem;
+    Movie(string name, int rating, double releaseYear, string review, string genre)
+        : name(name), rating(rating), releaseYear(releaseYear), review(review), genre(genre), next(nullptr) {}
+};
 
-    movieListSystem.runMovieSystem();
+class MovieList {
+private:
+    struct HashNode {
+        Movie* data;
+        HashNode* next;
+    };
 
+    HashNode* hashTable[TABLE_SIZE]; // Hash array
 
-    inputFile.close();
+public:
+    MovieList();
+    ~MovieList();
 
-    return 0;
-}
+    void runMovieSystem();
+    void addMovie();
+    void removeMovie();
+    void modifyMovie();
+    void displayMovieList();
+    void displayMoviesInRange();
+    void searchMovie();
+    void saveDataToFile();
+    void loadDataFromFile();
+    string movieRec();
+
+private:
+    int hashFunction(const string& key);
+    void insertIntoHashTable(Movie* movie);
+    void removeFromHashTable(const string& key);
+    Movie* searchHashTable(const string& key);
+    bool hashTableIsEmpty();
+};
